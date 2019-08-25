be97ff82ed9cae909b5f3c475ef6deac3978b24b
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2019-08-25 21:13:53 +0100

    [WIP] 'About us' page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   theme: {
     backgroundColor: theme => ({
       ...theme("colors"),
+      "gray-light": "#f9f9f9"
     }),
     extend: {
       screens: {
@@ -12,12 +13,14 @@ module.exports = {
           "dark": "#1f4d7b",
           "text-light": "#3e5a90",
           "default":"#357cb6",
+          "twitter": "#00aaec",
         },
         "orange": {
           "default": "#f7b38a",
           "shadow": "#cc875d",
         },
         "text-default": "#363636",
+        "text-gray-light": "#99adc1",
       },
       fontFamily: {
         body: ["Quicksand", "sans-serif"]
