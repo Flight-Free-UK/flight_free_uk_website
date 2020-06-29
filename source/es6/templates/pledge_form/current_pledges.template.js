a3fd9d58702abb872126ff699a8004976fa09476
Pete Stewart <pete.stewart3@gmail.com>
2020-06-29 20:26:05 +0100

    Some minor alignment and colour changes.

--- source/es6/templates/pledge_form/current_pledges.template.js
+++ source/es6/templates/pledge_form/current_pledges.template.js
@@ -1,6 +1,6 @@
 export default ({ target, member_count}) => `
 <h3 class="w-full text-2xl text-blue-dark mb-1 leading-tight text-center">Help us reach ${numberWithCommas(target)}!</h3>
-<p class="text-black text-center">${numberWithCommas(member_count)} have pledged so far</p>
+<p class="text-blue-dark text-center">${numberWithCommas(member_count)} have pledged so far</p>
 `;
 
 function numberWithCommas(x) {
