941954dbbf4278155812d2f3176befe19f81edfc
Pete Stewart <pete.stewart3@gmail.com>
2020-07-21 16:11:08 +0100

    Updated pledge form text & npm modules.

--- assets/source/js/templates/pledge_form/current_pledges.template.js
+++ assets/source/js/templates/pledge_form/current_pledges.template.js
@@ -1,5 +1,6 @@
 export default ({ target, member_count}) => `
-<h3 class="w-full text-2xl text-blue-dark mb-1 leading-tight text-center">Help us reach ${numberWithCommas(target)}!</h3>
+<h3 class="w-full text-xl text-blue-dark mb-1 leading-tight text-center">I will be flight free in ${new Date().getFullYear()}</h3>
+<p class="text-blue-dark text-center">Help us reach ${numberWithCommas(target)}!</p>
 <p class="text-blue-dark text-center">${numberWithCommas(member_count)} have pledged so far</p>
 `;
 
