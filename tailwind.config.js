19542a86bbc2788d96040039750d84b77407128a
Jake McMurchie <jake@jakemcmurchie.net>
2020-12-05 11:44:28 +0000

    Adjustments to mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,8 @@ module.exports = {
         blog: ["Helvetica", "Arial", "sans-serif"]
       },
       width: {
-        '300': '300px',
+        '280': '280px', // 288 - (2 x 4 rounded corners)
+        '288': '288px', // 320 - (2 x 16 margins)
         '320': '320px',
       },
       height: {
