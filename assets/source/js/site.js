dda9dce56774f546e6c65792fe6470fb21b55358
Jake McMurchie <jake@jakemcmurchie.net>
2021-02-09 16:35:55 +0000

    Removed debug info from site.js

--- assets/source/js/site.js
+++ assets/source/js/site.js
@@ -3,16 +3,12 @@
 //
 const progrs_win = document.getElementById( 'flightfreeuk-progress' );
 window.addEventListener("message", (event) => {
-    console.log( 'Reload instruction received: ' + event.data );
     if (event.origin !== "https://www.jakemcmurchie.net" && event.origin !== "https://flightfree.exigo.se") {
-        console.log( 'Unrecognised origin for message')
         return false;
     }
     if( 'reload' !== event.data ) {
-        console.log( 'Reload instruction not accepted');
         return false;
     } else {
-        console.log( 'Reload instruction accepted');
         progrs_win.src = progrs_win.src;
         return true;
     }
