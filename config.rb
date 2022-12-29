f60f8eff08d49dba22c3c608036d9068b89a4042
Jake McMurchie <jakemc@Jakes-MacBook-Pro.local>
2022-12-29 13:30:28 +0000

    Renamed 'how tos' to 'travel articles'

--- config.rb
+++ config.rb
@@ -62,14 +62,14 @@ end
 
 # the new how to section
 activate :blog do |blog|
-  blog.name = "how_to"
-  #blog.prefix = "how_to"
+  blog.name = "travel_articles"
+  #blog.prefix = "travel"
   blog.generate_day_pages = false
   blog.generate_month_pages = false
   blog.generate_year_pages = false
-  blog.permalink = "how_to/{title}.html"
-  blog.sources = "how_tos/{title}.html"
-  blog.layout = "blog_howto_post_layout"
+  blog.permalink = "travel/{title}.html"
+  blog.sources = "posts_travel/{title}.html"
+  blog.layout = "blog_post_travel_layout"
 end
 
 activate :external_pipeline,
