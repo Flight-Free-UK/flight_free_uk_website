8ce35765cb6ae8e19f02509376a602d618150466
Jake McMurchie <jakemc@Jakes-MacBook-Pro.local>
2022-12-29 15:04:11 +0000

    pointed pledge forms to Krystal

--- assets/source/js/site.js
+++ assets/source/js/site.js
@@ -1,24 +1,4 @@
 //
-// PLEDGE PROGRESS RELOADER
-//
-const progrs_win = document.getElementById("flightfreeuk-progress");
-window.addEventListener(
-	"message",
-	(event) => {
-		if (event.origin !== "https://www.jakemcmurchie.net" && event.origin !== "https://flightfree.exigo.se") {
-			return false;
-		}
-		if ("reload" !== event.data) {
-			return false;
-		} else {
-			progrs_win.src = progrs_win.src;
-			return true;
-		}
-	},
-	false
-);
-
-//
 // MYTHBUSTER
 //
 
