785a454a0665e1961ce884b945f0e94bd4f5fc92
Jake McMurchie <jakemc@Jakes-MacBook-Pro.local>
2023-01-16 11:20:22 +0000

    Added h-104 to purgecss whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     './source/**/*.erb',
   ],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  whitelist: ["ol", "figure", "figcaption", "blockquote", ".left-1/3"]
+  whitelist: ["ol", "figure", "figcaption", "blockquote", "left-1/3", "h-104"]
 })
 
 module.exports = {
